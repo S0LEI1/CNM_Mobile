32bfd33109bc8cee838fc11774f7f28868375478
S0LEI1 <vanngocpentakill@gmail.com>
2024-01-17 22:17:23 +0700

    update folder src

---
+++ src/components/component.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ComponentDemo = () => {
+  return (
+    <View>
+      <Text>ComponentDemo</Text>
+    </View>
+  )
+}
+
+export default ComponentDemo
