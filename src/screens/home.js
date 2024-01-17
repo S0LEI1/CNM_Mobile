1bad036b4989ead52d07056a099daf3f78ecd705
S0LEI1 <vanngocpentakill@gmail.com>
2024-01-17 21:40:10 +0700

    add icon-sax

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Heart } from 'iconsax-react-native'
 
 const HomeScreen = () => {
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Heart />
     </View>
   )
 }
